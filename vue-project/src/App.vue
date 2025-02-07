2dc02cb68e72c69d4139ac137239f111476ebf01
SaltyTea4924 <tanner.bauserman@bsu.edu>
2025-02-07 00:34:57 -0500

    Completed Assignment

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Order from './components/Order.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <Order/>
+    </div>
+  </header>
+
+</template>
+
---
+++ vue-project/src/components/Order.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>Onion {{ sum_onion }}</div>
+    <button @click="sum_onion++">+</button><button @click="minusClick_Onion">-</button>
+    <div>Tomatoes {{ sum_tomato }}</div>
+    <button @click="sum_tomato++">+</button><button @click="minusClick_Tomato">-</button>
+    <div>Chicken {{ sum_Chicken }}</div>
+    <button @click="sum_Chicken++">+</button><button @click="minusClick_Chicken">-</button>
+  </div>
+</template>
+
+<script>
+  export default{
+      data(){
+        return{
+          sum_onion: '0',
+          sum_tomato: '0',
+          sum_Chicken: '0'
+        }
+      },
+      methods:{
+        minusClick_Onion(){
+          if (this.sum_onion > 0){
+            this.sum_onion--;
+          }},
+          minusClick_Tomato(){
+          if (this.sum_tomato > 0){
+            this.sum_tomato--;
+          }
+        }, minusClick_Chicken(){
+          if (this.sum_Chicken > 0){
+            this.sum_Chicken--;
+          }
+
+      }}
+  }
+</script>
