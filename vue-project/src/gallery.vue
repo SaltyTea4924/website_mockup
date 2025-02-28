ca994f22c2e7390358c596f5169345cad5554c64
SaltyTea4924 <tanner.bauserman@bsu.edu>
2025-02-27 19:23:37 -0500

    Changed for new assignment

---
+++ vue-project/src/gallery.vue
@@ -0,0 +1,52 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+var guitar_images = ["guitar_1.jpg","guitar_2.jpg","guitar_3.jpg","guitar_4.jpg"]
+var pedal_images = ["pedal_1.jpg", "pedal_2.jpg","pedal_3.jpg","pedal_4.jpg"]
+</script>
+
+<template>
+  <header>
+
+    <div>
+      <HelloWorld msg="Gallery of guitars"/>
+    <div v-for = "guitar in guitar_images">
+            <img v-bind:src="guitar" height="200" width="125">
+    </div>
+    <HelloWorld msg ="Gallery of Pedals"/>
+    <div v-for = "pedal in pedal_images">
+      
+      <img v-bind:src="pedal" height="200" width="125"></div>
+    </div>
+  </header>
+
+  
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import gallery from './gallery.vue'
+
+createApp(gallery).mount('#app')
